67efac9a325b06107b6dbbb956973068f72e2de3
Miguel <a20122697@pucp.pe>
2019-10-26 11:49:32 -0500

    ddl_tablas.sql

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,5 +1,7 @@
 creacion de tablas SQL
---trabajando en rama alterna desarrollo
+--trabajando en rama alterna desarrollo- etiqueta nro 5
+--fecha: 20191026
+--inclusion de nologguing
 
 create table td_cliente (
 nombree varchar2(50)
